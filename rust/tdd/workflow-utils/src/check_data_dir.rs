dfc4506e5e7e8001d53939f46d1db8550dde2711
PiquoInterest <piquo@interesting.money>
2026-09-01 21:00:50 +0200

    feat(rust-utils): satisfy translated data-dir contract

--- rust/tdd/workflow-utils/src/check_data_dir.rs
+++ rust/tdd/workflow-utils/src/check_data_dir.rs
@@ -1,24 +1,3 @@
-use std::path::PathBuf;
-
-pub const POSSIBLE_WORKFLOW_DATA_PATHS: &[&str] =
-    &[".next/workflow-data", ".workflow-data", "workflow-data"];
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct DataDirContext {
-    pub cwd: PathBuf,
-    pub home: PathBuf,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct WorkflowDataDirResult {
-    pub data_dir: Option<PathBuf>,
-    pub project_dir: PathBuf,
-    pub short_name: String,
-    pub error: Option<String>,
-}
-
-#[must_use]
-pub fn find_workflow_data_dir(input: &str, context: &DataDirContext) -> WorkflowDataDirResult {
-    let _ = (input, context);
-    panic!("TDD RED: packages/utils/src/check-data-dir.test.ts implementation pending")
-}
+pub use workflow_utils::check_data_dir::{
+    DataDirContext, POSSIBLE_WORKFLOW_DATA_PATHS, WorkflowDataDirResult, find_workflow_data_dir,
+};
