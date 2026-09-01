22c024c11704f728aabd1a1058ffdb9f47a1369c
PiquoInterest <piquo@interesting.money>
2026-09-01 21:30:23 +0200

    feat(rust-utils): export pluralization contract

--- crates/workflow-utils/src/lib.rs
+++ crates/workflow-utils/src/lib.rs
@@ -2,6 +2,7 @@
 
 pub mod check_data_dir;
 pub mod parse_name;
+pub mod pluralize;
 
 pub use check_data_dir::{
     DataDirContext, POSSIBLE_WORKFLOW_DATA_PATHS, WorkflowDataDirResult, find_workflow_data_dir,
@@ -10,3 +11,4 @@ pub use parse_name::{
     ParsedName, format_step_name, format_workflow_name, parse_class_name, parse_step_name,
     parse_workflow_name, step_display_name, workflow_display_name,
 };
+pub use pluralize::pluralize;
