03e73e20f6fb2db979af7a34e1c2c4929fc06338
PiquoInterest <piquo@interesting.money>
2026-09-01 22:55:38 +0200

    feat(rust-utils): export secure debug contracts

--- crates/workflow-utils/src/lib.rs
+++ crates/workflow-utils/src/lib.rs
@@ -1,12 +1,14 @@
 #![forbid(unsafe_code)]
 
 pub mod check_data_dir;
+pub mod debug_log;
 pub mod parse_name;
 pub mod pluralize;
 
 pub use check_data_dir::{
     DataDirContext, POSSIBLE_WORKFLOW_DATA_PATHS, WorkflowDataDirResult, find_workflow_data_dir,
 };
+pub use debug_log::{DebugArgument, DebugSink, debug_log, is_workflow_debug_enabled};
 pub use parse_name::{
     ParsedName, format_step_name, format_workflow_name, parse_class_name, parse_step_name,
     parse_workflow_name, step_display_name, workflow_display_name,
